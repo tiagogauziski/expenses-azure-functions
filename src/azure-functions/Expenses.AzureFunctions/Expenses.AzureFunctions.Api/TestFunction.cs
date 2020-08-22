7566645c44061472ee9b278cdea8b3bd09c9a34e
Tiago Gauziski <tiago.gauziski@gmail.com>
2020-08-22 17:16:09 +1200

    renaming function

--- src/azure-functions/Expenses.AzureFunctions/Expenses.AzureFunctions.Api/TestFunction.cs
+++ src/azure-functions/Expenses.AzureFunctions/Expenses.AzureFunctions.Api/TestFunction.cs
@@ -10,9 +10,9 @@ using Newtonsoft.Json;
 
 namespace Expenses.AzureFunctions.Api
 {
-    public static class Function1
+    public static class TestFunction
     {
-        [FunctionName("Function1")]
+        [FunctionName("TestFunction")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
